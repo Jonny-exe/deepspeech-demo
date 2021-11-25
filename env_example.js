e2eca8e68f790ba5042b2930b3fbea30d5604b65
Jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-11-25 19:44:42 +0100

    fixed env file

---
+++ env_example.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PREFIX: ".",
+  PATH_SCORER_ES: "output_graph.pb",
+  PATH_SCORER_EN: "deepspeech-0.9.3-models.scorer",
+  PATH_MODEL_ES: "kenlm1.scorer",
+  PATH_MODEL_EN: "deepspeech-0.9.3-models.pbmm",
+  PATH_AUDIO: "test.wav"
+};
+
+const PATH = "/socket";
+export default PATH;
